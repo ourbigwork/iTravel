d63433a9e76040c3f472a17186c7a11f5ef9ca3d
shizia <260934316@qq.com>
2019-06-07 12:16:46 +0800

    还没搞完的，有些问题还在改

--- iTravel/UserInfo.cpp
+++ iTravel/UserInfo.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+#include "pch.h"                         //反正每次写之前加上四个std，加上clearscreen，结尾加东西，cout写成console
 #include "UserInfo.h"
 #include "CConsole.h"
 #include "constStr.h"
@@ -6,38 +6,69 @@ User::User(const std::string & Datafilename) {
 	file.ReopenFile(Datafilename);
 }
 void User::Login() {
-	using std::console;
+	using std::console;          //1、std::这种东西作用在于？
 	using std::endl;
 	using std::string;
 	using std::cin;
-	console.ClearScreen();
-	console << tUser << endl;
+	console.ClearScreen();       //清空屏幕当前内容（画的框框）
+	console << "用户名：";      //定义好的字符串，点开就明白了    2、inline？L？
 	string username, phone, password;
 	string _password;
 	cin >> username;
 	console << username << endl;
-	int ttype;
+	int ttype;      //判断是用户还是管理员
+	
 	file.findUser(username, password, phone, ttype);
-	if (password.empty()) {
-		console << tInvaildUser << endl;
-		return;
+	while (password.empty()) {
+		console << tInvaildUser;
+		cin >> username;
+		console << username << endl;
+		file.findUser(username, password, phone, ttype);
 	}
-	console << "密码:" << endl;
-	while (true) {
+	console << "密码:";
+	int i = 3;
+	while (i) {
 		_password = console.InputPassword();
 		console << "*********" << endl;
 		if (password != _password) {
-			console << "密码错误，请重新输入！" << endl;
+			i--;
+			if (!i) {
+				console << "尝试次数过多！" << endl;
+				return;
+			}
+			console << "密码错误，请重新输入：";
 		}
-		else
+		else {
 			break;
+		}
 	}
+
 	if (ttype == 2) {
 		console << "欢迎您，管理员:" << username << "." << endl;
 	}
 	else
 		console << "登陆成功！" << endl;
-	this->password = password;
+	//获得当前时间
+	string time = file.TimeUTCNow();
+	FILE* ptimeFile = fopen(".\\login", "a+");
+	//读入之前的文件；
+	char bLastTime[255]{ 0 };
+	if (ptimeFile) {
+		fscanf(ptimeFile, "%[^\n]%*c", bLastTime);//读取一行
+		if (strlen(bLastTime)) {
+			console << "上次登录：" << bLastTime << "." << endl;
+		}
+		else
+			OutputDebugStringA("[Warning]Last login time file not found!");
+		fclose(ptimeFile);
+	}
+	//清空之前的内容
+	ptimeFile = fopen(".\\login", "w");
+	if (ptimeFile) {
+		fprintf(ptimeFile, "%s from %s", time.c_str(), username.c_str());
+		fclose(ptimeFile);
+	}
+	this->password = password;     //给成员变量赋值
 	this->Phone = phone;
 	this->Username = username;
 	return;
@@ -54,22 +85,23 @@ void User::Regist() {
 	console.ClearScreen();
 	console << "注册" << endl;
 	string password1, password2, name;
-	string phone1, phone2;
+	string phone1, phone2;        //phone用户输入的暂时的号码
 	console << "请输入用户名：";
-	while (cin >> name) {
-		console << name << endl;
+	while (cin >> name)
+	{
+		console << name << endl;     
 		if (name.empty()) {
 			console << "用户名不能为空！" << endl;
-			cin.get();
+			cin.get();            //5、这个要来干啥？？get空的东西？
 			continue;
 		}
-		file.findUser(name, password2, phone2, ttype);
-		if (!password2.empty()) {
+		file.findUser(name, password2, phone2, ttype);          
+		if (!password2.empty()) {                      
 			console << "该用户已被注册，请重新输入" << endl << "请输入用户名：";
 			continue;
 		}
 		else {
-			password2.clear();
+			password2.clear();        //把password的字符删除
 			break;
 		}
 	}
@@ -78,20 +110,20 @@ void User::Regist() {
 		console << phone1 << endl;
 		if (phone1.length() != 11) {
 			console << "号码长度有误，请查证后重新输入" << endl << "请输入电话号码：";
-			cin.get();
+			cin.get();       //问题同5
 			continue;
 		}
 		else {
 			bool f = false;
-			for (auto x : phone1) {
-				if (!isdigit(x)) {
+			for (auto x : phone1) {    //对于phone1的每个元素x，auto是自动存储变量的关键字，自动判断类型
+				if (!isdigit(x)) {        //用来判断类型的
 					console << "号码必须全部是数字！" << endl;
 					f = true;
-					break;
+					break;        //跳出for循环但是跳不出while，继续输入号码
 				}
 			}
-			if (!f)
-				break;
+			if (!f)    
+				break; //号码全是数字，跳出while
 		}
 	}
 	console << "请输入密码（最多16位）：";
@@ -114,8 +146,7 @@ void User::Regist() {
 	}
 	console << "注册成功！" << endl;
 	int type = 1;
-	//不提供注册管理员的接口
-	file.RegistUser(name, password1, phone1, type);
+	file.RegistUser(name, password1, phone1, type);      
 }
 void User::Change() {
 	using std::console;
@@ -139,7 +170,7 @@ void User::Change() {
 			console << "密码尝试次数过多！" << endl;
 			return;
 		}
-		console << "密码错误，请重新输入,剩余次数：" << --i << "次；" << endl << "请输入密码：";
+		console << "密码错误，请重新输入,剩余次数：" << --i << "次；" << endl << "请输入密码：";    //减了再用
 		passwordcheck = console.InputPassword();
 		console << "*********" << endl;
 	}
@@ -160,42 +191,122 @@ void User::Change() {
 		console << "两次输入的密码不相同,再次输入密码：";
 		password2 = console.InputPassword();
 	}
-	file.revise(Username, phone, password1, type);
+	file.revise(Username, phone, password1, type);    //重要的函数
 	password = password1;
 	Phone = phone;
 	console << "修改成功！" << endl;
 	console << "修改后的信息：" << endl;
 	console << "用户名" << Username << '\n' << "手机号：" << phone << '\n' << "密码：" << password1 << endl;
 }
-
 void User::Logout() {
 	Phone.clear(), Username.clear(), password.clear();
 	return;
 }
 //------------------------------------------------------------------------------------------------
+void Customer::Regist()
+{
+	User::Regist();
+	fstream userlike,commentfile,replyfile;
+	string like_file_name, comment_file_name, reply_file_name;
+	like_file_name = Username + "的收藏夹";
+	comment_file_name = Username + "的历史评论";
+	reply_file_name = Username + "收到的回复";
 
-void Customer::show() {
-	using namespace std;
+	userlike.open("d:\\"+ like_file_name +".dat", ios::out | ios::binary | ios::app);
+
+	userlike.open("d:\\"+ comment_file_name+".dat", ios::out | ios::binary | ios::app);
+
+	userlike.open("d:\\"+ reply_file_name+".dat", ios::out | ios::binary | ios::app);
+
+	//初始化文件们????
 }
-
 void Customer::ilike() {
 	using namespace std;
+	using std::console;
+	using std::endl;
+	using std::string;
+	using std::cin;
+	console.ClearScreen();
+	fstream userlike;
+	string file_like_name;
+	file_like_name = Username + "的收藏夹";
+	string getwhat, getwhere;
+	                             //缺个获得景点信息的函数
+	                             //想要查重
+
+	userlike.open("d:\\" + file_like_name + ".txt", ios::out | ios::in | ios::binary | ios::app);
+	like.What = getwhat;
+	like.Where = getwhere;
+	userlike << like;
+	userlike.close();
 }
 
 void Customer::showlike() {
 	using namespace std;
+	using std::console;    
+	using std::endl;
+	using std::string;
+	using std::cin;
+	console.ClearScreen();
+	console << "我的收藏夹:" << endl;
+	fstream userlike;
+	string file_like_name;
+	file_like_name = Username + "的收藏夹";
+	userlike.open("d:\\" + file_like_name + ".txt", ios::out | ios::in | ios::binary);
+	userlike.seekg(0);
+	while (userlike >> readd)
+	{
+		console << readd.Where << "  " << readd.What << endl;
+	}
+	userlike.close();
 }
 
 void Customer::comment() {
 	using namespace std;
+	using std::console;
+	using std::endl;
+	using std::string;
+	using std::cin;
+	console.ClearScreen();
+	
 }
 
 void Customer::feedback() {
 	using namespace std;
+	using std::console;
+	using std::endl;
+	using std::string;
+	using std::cin;
+	console.ClearScreen();
+	fstream feedbackfile; //用户们共用一份
+	string word;
+	console << "请写下您的反馈" << endl;
+	cin >> word;
+	console << word;        
+							//缺一个展示在页面上的
+	myfeedback.customername = Username;
+	myfeedback.words = word;
+	string file_feedback_name;
+	file_feedback_name = "用户反馈";  //把评论从尾部写入文件
+	feedbackfile.open("d:\\" + file_feedback_name + ".txt", ios::out | ios::in | ios::binary | ios::app);
+	feedbackfile << myfeedback;
+	feedbackfile.close();
 }
 
 void Customer::read() {
 	using namespace std;
+	fstream replyfile; 
+	string word;
+	string file_reply_name;
+	file_reply_name = Username + "收到的回复";
+	replyfile.open("d:\\" + file_reply_name + ".txt", ios::out | ios::in | ios::binary);
+	replyfile.seekg(0);
+	console << "尊敬的" << Username<< endl;
+	while (replyfile >> myreply)
+	{
+		console << readd.What << endl;
+	}
+	replyfile.close();
 }
 //------------------------------------------------------------------------------------------------
 void Admin::set() {
@@ -208,4 +319,16 @@ void Admin::changeinformation() {
 
 void Admin::reply() {
 	using namespace std;
+	fstream feedbackfile;
+	fstream replyfile; //feedbackfile打开来读，写入reply
+	string file_feedback_name;string cname;//获得用户名
+	file_feedback_name = "用户反馈";
+	feedbackfile.open("d:\\" + file_feedback_name + ".txt", ios::out | ios::in | ios::binary );
+	
+	
+	
+	
+	string file_reply_name;
+	
+	file_reply_name =     + "收到的回复";
 }
--- iTravel/UserInfo.h
+++ iTravel/UserInfo.h
@@ -1,32 +1,70 @@
 #pragma once
 #ifndef USER_INFO
 #define USER_INFO
+#include<iostream>
 #include "pch.h"
 #include "FileHelper.h"
+using namespace std;
 struct User_like {
 	std::string Where;
-	std::string What;
+	std::string What;	
 };
+istream& operator>>(istream& in, User_like&p)
+	{
+		in >> p.Where>>p.What;             //读
+		return in;
+	}
+ostream& operator<<(ostream& out, const User_like& p)
+	{
+	out << p.Where << std::endl;
+	out << p.What << std::endl;            //写,一定要换行符！！！
+	return out;
+	}
+User_like like;
+User_like readd;
 struct Key_Data {
 	std::string nicknamee;//用户名
 	std::string phonee;//电话号码
 	std::string passwordd;//密码
-
 };
 struct FeedbackWords {
 	//用户反馈
 	std::string words;
 	std::string customername;
 };
+istream& operator>>(istream& in, FeedbackWords&p)
+{
+	in >> p.customername >> p.words;             //du
+	return in;
+}
+ostream& operator<<(ostream& out, const FeedbackWords& p)
+{
+	out << p.customername << std::endl; 
+	out<< p.words << std::endl;           //xie
+	return out;
+}
+FeedbackWords myfeedback;
 struct Replywords {
 	//管理员回信
 	std::string customername;
 	std::string words;
 };
-//用户类
+istream& operator>>(istream& in, Replywords&p)
+{
+	in >> p.customername >> p.words;             //du
+	return in;
+}
+ostream& operator<<(ostream& out, const Replywords& p)
+{
+	out << p.customername << std::endl;
+	out << p.words << std::endl;           //xie
+	return out;
+}
+FeedbackWords myreply;
+//使用者类
 class User {
 public:
-	User(const User&) = delete;
+	User(const User&) = delete;         //这几个是啥玩意
 	User(const User&&) = delete;
 	User(const std::string& filename);
 	void Login();
@@ -40,9 +78,10 @@ protected:
 	int type;//	1 - 普通用户；2  - 管理员
 	userDataFileHelper file;
 };
+//用户类
 class Customer :public User{
 public:
-	Customer() = default;//默认构造函数
+	void Regist();  //用来初始化文件
 	void show();         //个人信息显示
 	void ilike();        //加入心愿单,要对文档操作，把添加的东西转换成代号写入文档
 	void showlike();     //显示心愿单，要对文档操作，把文档里的代号读取出来,变成可视的
@@ -51,8 +90,8 @@ public:
 	void read();         //查看管理员的回复
 protected:
 	std::string preferencef, preferencep;
-	//int experience;   //升级……？还有经验点数
 };
+//管理员类
 class Admin :public User{
 public:
 	void set();
